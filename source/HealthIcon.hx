f6b66b036388e5aaddfd06d75b8bda6358b60cf3
AzriAndRanger <youareterr@gmail.com>
2021-08-14 11:45:54 +0300

    stincky source

---
+++ source/HealthIcon.hx
@@ -0,0 +1,70 @@
+package;
+
+import flixel.FlxSprite;
+
+class HealthIcon extends FlxSprite
+{
+	/**
+	 * Used for FreeplayState! If you use it elsewhere, prob gonna annoying
+	 */
+	public var sprTracker:FlxSprite;
+
+	public function new(char:String = 'bf', isPlayer:Bool = false)
+	{
+		super();
+		
+		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
+
+		antialiasing = true;
+		animation.add('bf', [0, 1], 0, false, isPlayer);
+		animation.add('bf-car', [0, 1], 0, false, isPlayer);
+		animation.add('bf-christmas', [0, 1], 0, false, isPlayer);
+		animation.add('bf-pixel', [21, 21], 0, false, isPlayer);
+		animation.add('spooky', [2, 3], 0, false, isPlayer);
+		animation.add('pico', [4, 5], 0, false, isPlayer);
+		animation.add('mom', [6, 7], 0, false, isPlayer);
+		animation.add('mom-car', [6, 7], 0, false, isPlayer);
+		animation.add('tankman', [8, 9], 0, false, isPlayer);
+		animation.add('face', [10, 11], 0, false, isPlayer);
+		animation.add('dad', [12, 13], 0, false, isPlayer);
+		animation.add('senpai', [22, 22], 0, false, isPlayer);
+		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
+		animation.add('spirit', [23, 23], 0, false, isPlayer);
+		animation.add('bf-old', [14, 15], 0, false, isPlayer);
+		animation.add('gf', [16], 0, false, isPlayer);
+		animation.add('gf-christmas', [16], 0, false, isPlayer);
+		animation.add('gf-pixel', [16], 0, false, isPlayer);
+		animation.add('parents-christmas', [17, 18], 0, false, isPlayer);
+		animation.add('monster', [19, 20], 0, false, isPlayer);
+		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
+		animation.add('GOC', [30, 31], 0, false, isPlayer);
+		animation.add('MTFUnitOne', [28, 29], 0, false, isPlayer);
+		animation.add('MTFUnitTwo', [28, 29], 0, false, isPlayer);
+		animation.add('MTFUnitThree', [28, 29], 0, false, isPlayer);
+		animation.add('Chaos', [32, 33], 0, false, isPlayer);
+		animation.add('SCPSlizka', [40, 41], 0, false, isPlayer);
+		animation.add('Azri', [16], 0, false, isPlayer);
+		animation.add('RubberFort', [38, 39], 0, false, isPlayer);
+		animation.add('Rubber', [38, 39], 0, false, isPlayer);
+		animation.add('Guard', [34, 35], 0, false, isPlayer);
+		
+		
+		animation.play(char);
+
+		switch(char)
+		{
+			case 'bf-pixel' | 'senpai' | 'senpai-angry' | 'spirit' | 'gf-pixel':
+				antialiasing = false;
+		}
+
+		scrollFactor.set();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (sprTracker != null)
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+	}
+}
